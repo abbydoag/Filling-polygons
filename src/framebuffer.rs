521a82b2bac3cfb0cb0cf78f21bcfe5a73c6f16f
abbydoag <abby.donis.a@gmail.com>
2024-07-22 18:37:41 -0600

    poligono 1 sin relleno

--- src/framebuffer.rs
+++ src/framebuffer.rs
@@ -1,72 +1,40 @@
 
-use std::fs::File;
-use std::io::{Write, BufWriter};
-
-use crate::framebuffer::Framebuffer;
-
-const BMP_HEADER_SIZE: usize = 54;
-const BMP_PIXEL_OFFSET: usize = 54;
-const BMP_BITS_PER_PIXEL: usize = 24;
-
-pub fn write_bmp_file(file_path: &str, buffer: &[u32], width: usize, height: usize) -> std::io::Result<()> {
-    let mut file = BufWriter::new(File::create(file_path)?);
-
-    write_bmp_header(&mut file, width, height)?;
-    write_pixel_data(&mut file, buffer, width, height)?;
-
-    Ok(())
+pub struct Framebuffer {
+    pub width: usize,
+    pub height: usize,
+    pub buffer: Vec<u32>,
+    background_color: u32,
+    current_color: u32,
 }
 
-fn write_bmp_header(file: &mut BufWriter<File>, width: usize, height: usize) -> std::io::Result<()> {
-    let file_size = (height * width * (BMP_BITS_PER_PIXEL / 8)) + BMP_HEADER_SIZE as usize;
-    let pixel_size = file_size - BMP_HEADER_SIZE;
-
-    // file header
-    file.write_all(b"BM")?;
-    file.write_all(&(file_size as u32).to_le_bytes())?;
-    file.write_all(&0u32.to_le_bytes())?;
-    file.write_all(&(BMP_PIXEL_OFFSET as u32).to_le_bytes())?;
-
-    // info header
-    file.write_all(&40u32.to_le_bytes())?;
-    file.write_all(&(width as u32).to_le_bytes())?;
-    file.write_all(&(height as u32).to_le_bytes())?;
-    file.write_all(&1u16.to_le_bytes())?;
-    file.write_all(&(BMP_BITS_PER_PIXEL as u16).to_le_bytes())?;
-    file.write_all(&0u32.to_le_bytes())?;
-    file.write_all(&(pixel_size as u32).to_le_bytes())?;
-    file.write_all(&0u32.to_le_bytes())?;
-    file.write_all(&0u32.to_le_bytes())?;
-    file.write_all(&0u32.to_le_bytes())?;
-    file.write_all(&0u32.to_le_bytes())?;
-
-    Ok(())
-}
-
-fn write_pixel_data(file: &mut BufWriter<File>, buffer: &[u32], width: usize, height: usize) -> std::io::Result<()> {
-    let padding_size = (4 - (width * BMP_BITS_PER_PIXEL / 8) % 4) % 4;
-    let padding = [0u8, 3];
-
-    for y in (0..height).rev() {
-        for x in 0..width {
-            let pixel = buffer[y * width + x];
-            let bgr = [(pixel >> 16) as u8, (pixel >> 8) as u8, pixel as u8];
-
-            file.write_all(&bgr)?;
+impl Framebuffer {
+    pub fn new(width: usize, height: usize) -> Self {
+        Framebuffer {
+            width,
+            height,
+            buffer: vec![0; width * height],
+            background_color: 0x000000,
+            current_color: 0xFFFFFF
         }
-
-        file.write_all(&padding[..padding_size])?;
     }
 
-    Ok(())
-}
+    pub fn clear(&mut self) {
+        for pixel in self.buffer.iter_mut() {
+            *pixel = self.background_color;
+        }
+    }
 
-pub trait WriteBmp {
-    fn render_buffer(&self, file_path: &str) -> std::io::Result<()>;
-}
+    pub fn point(&mut self, x: usize, y: usize) {
+        if x < self.width && y < self.height {
+            self.buffer[y * self.width + x] = self.current_color;
+        }
+    }
 
-impl WriteBmp for Framebuffer {
-    fn render_buffer(&self, file_path: &str) -> std::io::Result<()> {
-        write_bmp_file(file_path, &self.buffer, self.width, self.height)
+    pub fn set_background_color(&mut self, color: u32) {
+        self.background_color = color;
+    }
+
+    pub fn set_current_color(&mut self, color: u32) {
+        self.current_color = color;
     }
 }
---
+++ src/main.rs
@@ -0,0 +1,25 @@
+extern crate nalgebra_glm as glm;
+
+mod framebuffer;
+mod line;
+mod bmp;
+mod polygon1;
+
+use crate::framebuffer::Framebuffer;
+use crate::polygon1::Polygon;
+use crate::bmp::WriteBmp;
+
+fn main() {
+    let mut framebuffer = Framebuffer::new(800, 600);
+
+    framebuffer.set_background_color(0x000000);
+    framebuffer.clear();
+
+    framebuffer.set_current_color(0xFFFFFF);
+
+    let points1 = [(165 ,380), (185, 360), (180, 330), (207, 345), (233, 330), (230, 360), (250, 380), (220, 385), (205, 410), (193, 383)];
+    framebuffer.polygon(&points1);
+ 
+
+    let _ = framebuffer.render_buffer("output.bmp");
+}
