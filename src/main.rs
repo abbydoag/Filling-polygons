837f11f1c2341beed2865be9de01360236384dd6
abbydoag <abby.donis.a@gmail.com>
2024-07-24 16:48:46 -0600

    rellenos (1)

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,7 @@ fn main() {
     framebuffer.polygon(&[(165, 380), (185, 360), (180, 330), (207, 345), (233, 330), (230, 360), (250, 380), (220, 385), (205, 410), (193, 383)]);
 
     //relleno
-    framebuffer.set_fill_color(0xFF0000);
+    framebuffer.set_fill_color(0x00FFFF);
     framebuffer.filled_polygon(&points1);
 
     framebuffer.polygon(&[(165, 380), (185, 360), (180, 330), (207, 345), (233, 330), (230, 360), (250, 380), (220, 385), (205, 410), (193, 383)]);
