979e1bb0bf43aa36018a81af167c08718620d844
abbydoag <abby.donis.a@gmail.com>
2024-07-24 09:20:29 -0600

    Poligonos iniso 3 sin relleno

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,9 @@ fn main() {
 
     framebuffer.set_current_color(0xFFFFFF);
 
-    let points1 = [(165 ,380), (185, 360), (180, 330), (207, 345), (233, 330), (230, 360), (250, 380), (220, 385), (205, 410), (193, 383)];
-    framebuffer.polygon(&points1);
+    framebuffer.polygon(&[(165, 380), (185, 360), (180, 330), (207, 345), (233, 330), (230, 360), (250, 380), (220, 385), (205, 410), (193, 383)]);
+    framebuffer.polygon(&[(321, 335), (288, 286), (339, 251), (374, 302)]);
+    framebuffer.polygon(&[(377, 249), (411, 197), (436, 249)]);
  
 
     let _ = framebuffer.render_buffer("output.bmp");
