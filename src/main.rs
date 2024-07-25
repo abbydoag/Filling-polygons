001a917b6c2820f29bfa222cff2158bd29831d82
abbydoag <abby.donis.a@gmail.com>
2024-07-24 21:09:00 -0600

    Rellenos poligonos (colores aun no coinciden)

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ mod line;
 mod bmp;
 mod polygon1;
 
+use glm::Vec3;
+
 use crate::framebuffer::Framebuffer;
 use crate::polygon1::Polygon;
 use crate::bmp::WriteBmp;
@@ -15,12 +17,49 @@ fn main() {
     framebuffer.set_background_color(0x000000);
     framebuffer.clear();
 
+    let points1 = vec![
+        Vec3::new(165.0, 380.0, 0.0),
+        Vec3::new(185.0, 360.0, 0.0),
+        Vec3::new(180.0, 330.0, 0.0),
+        Vec3::new(207.0, 345.0, 0.0),
+        Vec3::new(233.0, 330.0, 0.0),
+        Vec3::new(230.0, 360.0, 0.0),
+        Vec3::new(250.0, 380.0, 0.0),
+        Vec3::new(220.0, 385.0, 0.0),
+        Vec3::new(205.0, 410.0, 0.0),
+        Vec3::new(193.0, 383.0, 0.0),
+    ];
+    
+    //orilla P1
     framebuffer.set_current_color(0xFFFFFF);
-
     framebuffer.polygon(&[(165, 380), (185, 360), (180, 330), (207, 345), (233, 330), (230, 360), (250, 380), (220, 385), (205, 410), (193, 383)]);
+
+    //relleno p1
+    framebuffer.set_fill_color(0x00FFFF);
+    framebuffer.filled_polygon(&points1);
+
+    let points2 = vec![
+        Vec3::new(321.0, 335.0, 0.0),
+        Vec3::new(288.0, 286.0, 0.0),
+        Vec3::new(339.0, 251.0, 0.0),
+        Vec3::new(374.0, 302.0, 0.0)
+    ];
+
+    framebuffer.set_current_color(0xFFFFFF);
     framebuffer.polygon(&[(321, 335), (288, 286), (339, 251), (374, 302)]);
+    framebuffer.set_fill_color(0xFF0000);
+    framebuffer.filled_polygon(&points2);
+
+
+    let points3 = vec![
+        Vec3::new(377.0, 249.0, 0.0), 
+        Vec3::new(411.0, 197.0, 0.0), 
+        Vec3::new(436.0, 249.0, 0.0)
+    ];
+    framebuffer.set_current_color(0xFFFFFF);
     framebuffer.polygon(&[(377, 249), (411, 197), (436, 249)]);
- 
+    framebuffer.set_fill_color(0x0000FF);
+    framebuffer.filled_polygon(&points3);
 
     let _ = framebuffer.render_buffer("output.bmp");
 }
